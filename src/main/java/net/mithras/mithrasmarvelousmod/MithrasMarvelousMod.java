78721366b7f1e0fb3d478425246e729d7a5499a1
Mithras <mithrasishere@gmail.com>
2026-06-22 16:22:46 -0300

    second try commit.

---
+++ src/main/java/net/mithras/mithrasmarvelousmod/MithrasMarvelousMod.java
@@ -0,0 +1,24 @@
+package net.mithras.mithrasmarvelousmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MithrasMarvelousMod implements ModInitializer {
+	public static final String MOD_ID = "mithrasmarvelousmod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.fromNamespaceAndPath(MOD_ID, path);
+	}
+}
