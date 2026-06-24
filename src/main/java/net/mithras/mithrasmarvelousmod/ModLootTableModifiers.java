3531d4ed60a68e87f933507263f31aec08c69d5c
Mithras <mithrasishere@gmail.com>
2026-06-24 18:35:24 -0300

    potential fix for loot table.

--- src/main/java/net/mithras/mithrasmarvelousmod/ModLootTableModifiers.java
+++ src/main/java/net/mithras/mithrasmarvelousmod/ModLootTableModifiers.java
@@ -2,6 +2,7 @@ package net.mithras.mithrasmarvelousmod;
 
 import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.minecraft.resources.Identifier;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
 import net.mithras.mithrasmarvelousmod.item.ModItems;
@@ -9,7 +10,7 @@ import net.minecraft.data.loot.packs.*;
 
 public class ModLootTableModifiers {
 
-    private static final Identifier ACACIA_LEAVES_LOOT_TABLE_ID = new Identifier("minecraft", "blocks/acacia_leaves");
+    private static final Identifier ACACIA_LEAVES_LOOT_TABLE_ID = Identifier.fromNamespaceAndPath("minecraft", "blocks/acacia_leaves");
 
     public static void modifyLootTables() {
     LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
